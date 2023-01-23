28c87fe79e085ab2aa0b58442812190d3524b2c2
Никита Федоров <n.fdrv@list.ru>
2023-01-23 19:24:51 +0300

    Сделал user_api

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
 )
 
 from .views import UserViewSet
@@ -12,5 +11,7 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/',
+         TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, filters, generics
+from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
+
 
 from .serializer import UserSerializer
 

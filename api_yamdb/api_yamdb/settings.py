3a17f739620d9144825b37a47f686b42eba99864
Никита Федоров <n.fdrv@list.ru>
2023-01-27 14:51:03 +0300

    исправил код по тестам на сайте

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -19,6 +19,8 @@ AUTH_USER_MODEL = 'users.User'
 
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -29,8 +31,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
     'django_filters',
-    'rest_framework',
-    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
